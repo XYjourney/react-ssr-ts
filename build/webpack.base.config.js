3fd7c713f9314b9aef5ed8b67cf695ac2f4eaee2
Xiaowei Xie <xiaowxie@microsoft.com>
2021-08-06 17:38:55 +0800

    first commit

---
+++ build/webpack.base.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('./utils');
+
+module.exports = {
+    entry: resolve('app/main.ts'),
+    output: {
+        path: resolve('dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+              test: /\.tsx?$/,
+              loader: 'ts-loader',
+              exclude: /node_modules/,
+            },           
+            {
+                test: /\.(js|jsx)$/,
+                // 只编译app文件夹下的文件
+                include: resolve('app'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                        ],
+                    }
+                }
+            },
+        ]
+    },
+    resolve: {
+        // 设置路径别名
+        alias: {
+            '@': resolve('app'),
+        },
+        // 文件后缀自动补全, 就是你import文件的时候如果没写后缀名就会优先找下面这几个
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    // 第三方依赖，可以写在这里，不打包
+    externals: {},
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: resolve('template/app.html')
+        })
+    ],
+    devServer:{
+      contentBase:resolve('.'),
+      open: true,
+      port: 9000
+    }
+}
